19fbe9438e9e0fecadcd2d57404c8eb45fab5da1
BrunoSobralDEV <brunosobralss@hotmail.com>
2021-12-15 17:59:05 -0300

    saldo

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -6,13 +6,28 @@ import {
 } from "./styles";
 
 import Header from "../../components/Header";
+import Card from "../../components/Card";
 
 const Dashboard = () => {
+  const wallet = 5000;
   return (
     <DashboardBackground>
       <Header />
       <BodyContainer>
-        
+        <div>
+          <Card noShadow width="90%">
+            <InlineTitle>
+              <h2 className="h2">Saldo Atual</h2>
+            </InlineTitle>
+              <h3 className="wallet">
+                {wallet.toLocaleString('pt-BR', {style: 'currency', currency:"BRL"})}
+              </h3>
+            
+          </Card>
+        </div>
+        <div>
+
+        </div>
       </BodyContainer>
     </DashboardBackground>
   );
